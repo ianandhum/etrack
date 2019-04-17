b7506587aa58a4935dbe1795f63bb7487ab59304
Anandhu Manoj <anandhumanoj282@gmail.com>
2019-04-17 13:44:08 +0530

    aaded Comments in Home.js

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -14,6 +14,7 @@ import Loading from '../components/util/Loading'
 
 import { initTasks as initActiveTasks } from '../data/actions/active_tasks';
 
+//Home Route
 class Home extends Component{
     componentDidMount(){
         if(!this.props.loaded)
