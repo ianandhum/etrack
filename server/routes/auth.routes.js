e9a180cfcc641ac65b269219e0b26dda8a793cbc
Anandhu Manoj <anandhumanoj282@gmail.com>
2019-07-21 14:52:34 +0530

    New route added for failed authentication redirects

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,12 +6,20 @@ module.exports=function(router,passport){
         successRedirect: '/api/',
         failureRedirect: '/api/failed'
     }));
+
     
     router.route('/logout').get(isAuthenticated,function(req,res){
         req.logout();
         res.send({
             status:true
         })
+    });
+
+    router.route('/permission-denied').get(function (req,res) {
+        res.status(403).send({
+            "status":403,
+            "message":"Permission denied"
+        });
     });
     
     if(process.env.NODE_ENV !== 'production'){
