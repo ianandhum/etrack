48dd825bb00ca3f2be849b72842637d7ba5c2fc2
Anandhu Manoj <anandhumanoj282@gmail.com>
2019-07-21 14:50:29 +0530

    modified auth failure redirect URL

--- server/helpers/auth/isAuthenticated.js
+++ server/helpers/auth/isAuthenticated.js
@@ -2,5 +2,5 @@ module.exports = function (req, res, next) {
 	if (req.isAuthenticated())
         return next();
     //not loggedin
-	res.redirect('/api/not-logged');
+	res.redirect('/api/permission-denied');
 }
